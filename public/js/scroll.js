61deba799048cf97f0caf61d4d03da3b7a3ec223
YutaTakeuchi1 <1718080.ty@sist.ac.jp>
2023-05-28 22:43:28 +0900

    スクロールイベント修正

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,17 +1,35 @@
+//クリックで下へスクロール
 function scrollToBottom(posScholar) {
   let downArrow=document.getElementById("arrows");
 
   // 最下部の位置を取得
   const pos = window.pageYOffset + window.innerHeight;
 
-  if(pos <= document.documentElement.scrollHeight - window.innerHeight*2){
+  if(0 >= document.documentElement.scrollHeight - pos){
     window.scrollTo({ top: pos, behavior: 'smooth' });
+    downArrow.style.display="none";
+    console.log("ページの最下部にスクロールされました自動");
   }
   else{
     window.scrollTo({ top: pos, behavior: 'smooth' });
-    downArrow.style.display="none";
   }
 }
+
+//TODO: スクロールイベントの性質上イベントが複数回走る対応
+//マウスでのスクロール時の自動スクロールボタンの表示非表示
+window.addEventListener('scroll', function() {
+  let downArrow=document.getElementById("arrows");
+  // 最下部の位置を取得
+  const pos = window.pageYOffset + window.innerHeight;
+  if (0 >= document.documentElement.scrollHeight - pos) {
+    // ページの最下部にスクロールされた場合の処理をここに記述します
+    console.log("ページの最下部にスクロールされました手動");
+    downArrow.style.display="none";
+  }
+  else{
+    downArrow.style.display="block";
+  }
+});
 
 function scrollToTop() {
   let downArrow=document.getElementById("arrows");
