4cf0a9a04b8f0631fee3d997fa24080db38602d3
YutaTakeuchi1 <1718080.ty@sist.ac.jp>
2023-06-17 18:59:34 +0900

    文言修正とスクロールイベント修正

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,3 +1,6 @@
+//スクロールイベント内の処理が全てのスクロールイベントで実行されないように制御
+let scrollEventAdjustFlg;
+
 //クリックで下へスクロール
 function scrollToBottom(posScholar) {
   let downArrow=document.getElementById("arrows");
@@ -13,20 +16,23 @@ function scrollToBottom(posScholar) {
   }
 }
 
-//TODO: スクロールイベントの性質上イベントが複数回走る対応
 //マウスでのスクロール時の自動スクロールボタンの表示非表示
 window.addEventListener('scroll', function() {
-  let downArrow=document.getElementById("arrows");
-  // 最下部の位置を取得
-  const pos = window.pageYOffset + window.innerHeight;
-  if (0 >= document.documentElement.scrollHeight - pos) {
-    //スクロールしてページ最下部へ到達したとき、自動スクロール非表示
-    downArrow.style.display="none";
-  }
-  else{
-    //ページ最下部から上にスクロールしたとき、下へ移動する自動スクロール復活
-    downArrow.style.display="block";
-  }
+  if ( scrollEventAdjustFlg ) return ;
+  scrollEventAdjustFlg = setTimeout(function (){
+    scrollEventAdjustFlg = 0;
+    let downArrow=document.getElementById("arrows");
+    // 最下部の位置を取得
+    const pos = window.pageYOffset + window.innerHeight;
+    if (0 >= document.documentElement.scrollHeight - pos) {
+      //スクロールしてページ最下部へ到達したとき、自動スクロール非表示
+      downArrow.style.display="none";
+    }
+    else{
+      //ページ最下部から上にスクロールしたとき、下へ移動する自動スクロール復活
+      downArrow.style.display="block";
+    }
+  }, 1000);
 });
 
 //ページ最上部へ移動
